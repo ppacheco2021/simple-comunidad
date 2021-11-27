82bbd1706a841d36132991746e5891551a76ec84
PPACHECO <patricioa.pacheco@gmail.com>
2021-11-26 21:21:53 -0300

    commit scaffold productos

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_26_232624) do
+ActiveRecord::Schema.define(version: 2021_11_27_001708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "quantity"
+    t.string "imagen"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
